eb64ae30f9c438c741a1800e6ad63361d1d7ea8e
Evans Gacheru <evans.gacheru.munene@gmail.com>
2024-04-08 20:12:51 +0300

    dashboard updates

---
+++ src/config/resources.tsx
@@ -0,0 +1,33 @@
+import { DashboardOutlined, ProjectOutlined, ShopOutlined } from '@ant-design/icons'
+import { IResourceItem } from '@refinedev/core'
+export const resources: IResourceItem[] = [
+    {
+        name: 'dashboard',
+        list: '/',
+        meta: {
+            lable: 'Dashboard',
+            icon: <DashboardOutlined />
+        },
+    },
+    {
+        name: 'companies',
+        list: '/companies',
+        show: '/companies/:id',
+        create: '/companies/new',
+        edit: '/companies/edit/:id',
+        meta:{
+            lable: 'Companies',
+            icon: <ShopOutlined />
+        }
+    },
+    {
+        name: 'tasks',
+        list: '/tasks',
+        create: '/task/new',
+        edit: '/tasks/edit/:id',
+        meta: {
+            lable: 'Tasks',
+            icon: <ProjectOutlined />
+        }
+    }
+]
